8b9d3b8dabc68e9592170a560607a7ec4b4ce5eb
s1022993 <t91p65a@gmail.com>
2016-07-20 17:03:32 +0800

    can receive data

--- app/src/main/java/com/example/toshiba/ardiuno_bluetooth/ledControl.java
+++ app/src/main/java/com/example/toshiba/ardiuno_bluetooth/ledControl.java
@@ -6,26 +6,40 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
+import java.util.logging.LogRecord;
+
 public class ledControl extends AppCompatActivity {
 
-    Button btnOn, btnOff, btnDis;
+    Button btnOn, btnOff, btnDis,btnRec;
     SeekBar brightness;
     String address = null;
     private ProgressDialog progress;
     BluetoothAdapter myBluetooth = null;
     BluetoothSocket btSocket = null;
-    InputStream tmpIn = null;
+    InputStream mmIntputStream;
+    Thread workerThread;
+    Handler h;
+    final int RECIEVE_MESSAGE = 1;		// Status  for Handler
+    private StringBuilder sb = new StringBuilder();
+    TextView txv2;
+
+    volatile boolean stopWorker;
+    byte[] readBuffer;
+    int readBufferPosition;
     private boolean isBtConnected = false;
     static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     @Override
@@ -42,13 +56,14 @@ public class ledControl extends AppCompatActivity {
         btnOn = (Button)findViewById(R.id.btnOn);
         btnOff = (Button)findViewById(R.id.btnOff);
         btnDis = (Button)findViewById(R.id.btnDis);
+        btnRec = (Button)findViewById(R.id.btnRec);
         brightness = (SeekBar)findViewById(R.id.seekBar);
+        txv2=(TextView)findViewById(R.id.textView2);
 
         new ConnectBT().execute(); //Call the class to connect
         btnOn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 turnOnLed();      //method to turn on
             }
         });
@@ -66,26 +81,35 @@ public class ledControl extends AppCompatActivity {
                 Disconnect(); //close connection
             }
         });
+        btnRec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                beginReceiveDatas();
+            }
+        });
 
         brightness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if (fromUser==true) {
+                if (fromUser == true) {
                     try {
-                        btSocket.getOutputStream().write(String.valueOf(progress).getBytes ());
-                    }
-                    catch (IOException e) {
+                        btSocket.getOutputStream().write(String.valueOf(progress).getBytes());
+                    } catch (IOException e) {
 
                     }
                 }
             }
+
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
             }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+
+
     }
     private class ConnectBT extends AsyncTask<Void, Void, Void> {
         private boolean ConnectSuccess = true;
@@ -139,7 +163,7 @@ public class ledControl extends AppCompatActivity {
     private void turnOffLed(){
         if (btSocket!=null){
             try{
-                btSocket.getOutputStream().write("TF".toString().getBytes());
+                btSocket.getOutputStream().write("B".toString().getBytes());
             }
             catch (IOException e){
                 msg("Error");
@@ -149,14 +173,73 @@ public class ledControl extends AppCompatActivity {
     private void turnOnLed(){
         if (btSocket!=null){
             try{
-                btSocket.getOutputStream().write("TO".toString().getBytes());
+                btSocket.getOutputStream().write("A".toString().getBytes());
             }
             catch (IOException e){
                 msg("Error");
             }
         }
     }
+    private void beginReceiveDatas(){
+        try{
+            btSocket.getOutputStream().write("C".toString().getBytes());
+        }
+        catch (IOException e){
+            msg("Error");
+        }
+
+        //final Handler handler=new Handler();
+        final byte delimiter=10;
+        stopWorker=false;
+        readBufferPosition=0;
+        readBuffer=new byte[1024];
+        try {
+            mmIntputStream=btSocket.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        workerThread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (!Thread.currentThread().isInterrupted()&&!stopWorker){
+                    try {
+                        int bytesAvailable=mmIntputStream.available();
+                        if(bytesAvailable>0){
+                            byte[] packetBytes=new byte[bytesAvailable];
+
+                            String str1="";
+                            mmIntputStream.read(packetBytes);
+                            String str2=new String(packetBytes,"UTF-8");
+                            str1=str1+packetBytes[0];
+                            Log.e("in",  str2);
+                            for(int i=0;i<bytesAvailable;i++){
+                                byte b=packetBytes[i];
+                                if(b==delimiter){
+                                    byte[] encodeBytes=new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer,0,encodeBytes,0,encodeBytes.length);
+                                    final String data=new String(encodeBytes,"US-ASCII");
+                                    readBufferPosition=0;
+
+                                }
+                                else {
+                                    readBuffer[readBufferPosition++]=b;
+                                }
+                            }
+
+
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        workerThread.start();
+    }
     private void msg(String s) {
         Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
     }
+
+
 }
